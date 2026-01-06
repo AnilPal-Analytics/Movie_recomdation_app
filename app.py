3dbed59ef8224c03d60540acfb49832256671b0d
AnilPal-Analytics <anilpal.ds@gmail.com>
2026-01-06 13:10:25 +0530

    Final-commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+import os
+load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_GEMINI_API"))
+
+st.title("📽️Movie Recommendation System")
+
+user_input = st.text_input("Enter the movie name")
+submit = st.button("Submit...")
+
+model = genai.GenerativeModel("gemini-2.5-flash-lite")
+
+if submit and user_input.strip():
+    st.success(f"Movie name entered: {user_input}")
+
+    response = model.generate_content(
+        f"Generate movie recommendations related to: {user_input}"
+    )
+
+    st.write("🎬 **Related Recommendations:**")
+    st.write(response.text)
+
+else:
+    st.warning("You need to enter the movie name")
